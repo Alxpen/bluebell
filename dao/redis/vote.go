961dadec466182d85b5442db5ce27b408b712a28
Alxpen <1254423662@qq.com>
2026-08-03 20:55:22 +0800

    commit_vote

--- dao/redis/vote.go
+++ dao/redis/vote.go
@@ -1,10 +1,22 @@
 package redis
 
 import (
+	"errors"
+	"math"
 	"strconv"
 	"time"
 
 	"github.com/go-redis/redis"
+)
+
+const (
+	oneWeekInSeconds = 7 * 24 * 3600
+	scorePerVote     = 432
+)
+
+var (
+	ErrVoteTimeExpire = errors.New("投票时间已过")
+	ErrVoteRepeated   = errors.New("不允许重复投票")
 )
 
 func CreatePost(postID, communityID int64) error {
@@ -22,6 +34,44 @@ func CreatePost(postID, communityID int64) error {
 	//add id to set of community
 	cKey := getRedisKey(KeyCommunitySetPF + strconv.Itoa(int(communityID)))
 	pipeline.SAdd(cKey, postID)
+	_, err := pipeline.Exec()
+	return err
+}
+
+func VoteForPost(userID, postID string, value float64) error {
+	//if expire
+	postTime := client.ZScore(getRedisKey(KeyPostTimeZSet), postID).Val()
+	if float64(time.Now().Unix())-postTime > oneWeekInSeconds {
+		return ErrVoteTimeExpire
+	}
+
+	//search history
+	ov := client.ZScore(getRedisKey(KeyPostVoteZSetPF+postID), userID).Val()
+
+	if value == ov {
+		return ErrVoteRepeated
+	}
+
+	var op float64
+	if value > ov {
+		op = 1
+	} else {
+		op = -1
+	}
+	diff := math.Abs(ov - value)
+
+	pipeline := client.TxPipeline()
+	pipeline.ZIncrBy(getRedisKey(KeyPostVoteZSetPF), op*diff*scorePerVote, postID)
+
+	if value == 0 {
+		pipeline.ZRem(getRedisKey(KeyPostVoteZSetPF+postID), userID)
+	} else {
+		pipeline.ZAdd(getRedisKey(KeyPostVoteZSetPF+postID), redis.Z{
+			Score:  value,
+			Member: userID,
+		})
+	}
+
 	_, err := pipeline.Exec()
 	return err
 }
